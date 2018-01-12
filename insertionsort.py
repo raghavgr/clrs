b43b8aa03f1cbff587f7d58af59fdec61de85376
raghavgr94 <raghavgr94@gmail.com>
2018-01-11 18:22:25 -0800

    :bulb: Inertion sort implemented

---
+++ insertionsort.py
@@ -0,0 +1,12 @@
+def insertion_sort(arr):
+    for i in range(1, len(arr)):
+        key = arr[i]
+        j = i - 1
+        while j >= 0 and arr[j] > key:
+            arr[j + 1] = arr[j]
+            j = j - 1
+        arr[j + 1] = key
+
+arr = [65, 22, 34,21, 55, 21, 10]
+insertion_sort(arr)
+print(arr)
