19e04bd6033daba204362d88ad086e55db4ad955
raghavgr94 <raghavgr94@gmail.com>
2018-01-13 18:17:52 -0800

    :heavy_check_mark: Recursive implementation done

--- binary_search.py
+++ binary_search.py
@@ -11,4 +11,15 @@ def iter_binary_search(arr, value):
             high = mid - 1
     return None
 
-print(iter_binary_search([1, 3, 4, 5, 6, 9], 4))
+def binary_search(arr, value, low, high):
+    mid = (low + high) // 2
+    if value == arr[mid]:
+        return mid
+    elif value > arr[mid]:
+        return binary_search(arr, value, mid + 1, high)
+    else:
+        return binary_search(arr, value, low, mid - 1)
+
+test_arr = [1, 3, 4, 5, 6, 9]
+print(iter_binary_search(test_arr, 4))
+print(binary_search(test_arr, 4, 0, len(test_arr) ))
