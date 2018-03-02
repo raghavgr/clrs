9780492994c34be68fcca60ae1a9c4853eb81055
raghavgr94 <raghavgr94@gmail.com>
2018-03-02 11:59:39 -0800

    :horse_racing: Implemented memoized top down approach for rod cutting problem

--- ch15_dynamic_programming/rod_cutting.py
+++ ch15_dynamic_programming/rod_cutting.py
@@ -20,7 +20,32 @@ def cut_rod(prices, n):
     if n == 0:
         return 0
     q = -sys.maxsize - 1
-    for i in range (1, n):
+    for i in range(n):
         q = max(q, prices[i] + cut_rod(prices, n - i))
     return q 
 
+def memoized_cut_rod(prices, n):
+    """
+    Memoized cut rod. 
+    Initialize r, an auxiliary array for revenue
+    """
+    r = [-1 for i in range(n)]
+    return memoized_cut_rod_aux(prices, n, r)
+
+def memoized_cut_rod_aux(prices, n, r):
+    """
+    Helper method for memoized approach
+    top-down
+    """
+    if r[n- 1] >= 0:        # once result found, don't check again.
+        return r[n - 1]
+    if n == 0:
+        q = 0
+    else:
+        q = -sys.maxsize - 1
+        for i in range(0, n):
+            q = max(q, prices[i] + memoized_cut_rod_aux(prices, n - i - 1, r))
+    r[n - 1] = q
+    return q
+
+print(memoized_cut_rod([1, 5, 8, 9], 4))
