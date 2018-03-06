b6e27b623551a11bd8a0303375b832a7f30ae883
raghavgr94 <raghavgr94@gmail.com>
2018-03-06 11:39:58 -0800

    :heavy_check_mark: finished coin change with dp and extended_dp

--- ch15_dynamic_programming/coin_change.py
+++ ch15_dynamic_programming/coin_change.py
@@ -48,4 +48,42 @@ def recurCoinChangeMemo(coinValList, change, knownResults):
                 knownResults[change] = minCoins
     return minCoins
 
-print(recurCoinChangeMemo([1, 2, 5], 11, [0]*12))
+def dpMakeChange(coinValList, change, minCoins):
+    """
+    Dynamic programming version
+    Returns optimal value but not the solution
+    """
+    for cents in range(change + 1):
+        coinCount = cents
+        for j in [c for c in coinValList if c <= change]:
+            if minCoins[cents - j] + 1 < coinCount:
+                coinCount = minCoins[cents - j] + 1
+        minCoins[cents] = coinCount
+        #print(minCoins)
+    
+    return minCoins[change]
+
+def extended_dpMakeChange(coinValList, change, minCoins, coinsUsed):
+    for cents in range(change + 1):
+        coinCount = cents
+        newCoin = 1
+        for j in [c for c in coinValList if c <= change]:
+            if minCoins[cents - j] + 1 < coinCount:
+                coinCount = minCoins[cents - j] + 1
+                newCoin = j
+        minCoins[cents] = coinCount
+        coinsUsed[cents] = newCoin
+    printCoins(coinsUsed, change)
+    return minCoins[change]
+
+def printCoins(coinsUsed, change):
+    print(coinsUsed)
+    remaining = change
+    while remaining > 0:
+        curr = coinsUsed[remaining]
+        print(curr)
+        remaining -= curr
+print(extended_dpMakeChange([1, 5, 10], 11, [0]*12, [0]*12))
+print(extended_dpMakeChange([2], 3, [0]*4, [0]*4))
+
+
